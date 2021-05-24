8367ef73beec89f07d0274423bf33a4f7ffd8b1c
Nalivayko Polina <71880900+Nalivayko13@users.noreply.github.com>
2021-05-24 17:49:05 +0300

    feat: add  fun calculateFairlySplit

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -2,9 +2,16 @@ package subtask2
 
 class BillCounter {
 
-    // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val annBill = bill.toMutableList()
+        annBill.removeAt(k);
+        var sum = annBill.sum()
+        sum /= 2 //должна заплатить тётька
+        return if(b==sum){
+            "Bon Appetit"
+        }else{
+            (b-sum).toString()
+        }
     }
 }
