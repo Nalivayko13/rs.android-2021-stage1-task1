1dc320240c167e6fe6cb188b4fbde777c5943e9b
Nalivayko Polina <71880900+Nalivayko13@users.noreply.github.com>
2021-05-25 16:12:48 +0300

    feat: add   fun getResult

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,39 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
+    private val brackets = mapOf('(' to ')', '<' to '>', '[' to ']')
+
+    object BracketWithIndex {
+        val <X> Pair<X, *>.bracket get() = first
+        val <Y> Pair<*, Y>.index get() = second
+    }
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val openBrackets = ArrayList<Pair<Char, Int>>()
+        val result = mutableListOf<Pair<String, Int>>()
+
+        for (index in inputString.indices) {
+            val character = inputString[index]
+
+            if (brackets.containsKey(character)) {
+                openBrackets.add(character to index)
+            }
+            else if (brackets.containsValue(character)) {
+                with(BracketWithIndex) {
+                    for (openBracket in openBrackets.reversed()) {
+                        
+                        if (brackets[openBracket.bracket] == character) {
+                            val substring = inputString.slice(openBracket.index + 1 until index)
+                            result.add(substring to openBracket.index)
+                            openBrackets.remove(openBracket)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        result.sortBy { it.second }
+        return result.map { it.first }.toTypedArray()
     }
 }
+
